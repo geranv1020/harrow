7ad95470b3da0a2cdc6907e32da1aa60795b0f56
peterugah <p.ugah.pu@gmail.com>
2023-09-14 16:41:49 +0100

    feat: added changes

--- harrow-science-app/src/App.js
+++ harrow-science-app/src/App.js
@@ -1,22 +1,22 @@
 import React from "react";
-import { BrowserRouter,Routes,Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import Clinical from "./pages/clinical-data";
 import Investigator from "./pages/investigator";
 import MedInfo from "./pages/med-info";
 import Char from "./pages/char-grant";
 
-const App=()=>{
-   return(
-      <BrowserRouter>
-      <Routes>
-      <Route path="/" element={<Home/>} />
-         {/* <Route path="/clinicaldata" element={<Clinical/>} />
-         <Route path="/iit" element={<Investigator/>} />
-         <Route path="/medinfo" element={<MedInfo/>} />
-         <Route path="/charitablegrant" element={<Char/>} /> */}
-      </Routes>
-      </BrowserRouter>
-   )
-}
-export default App
+const App = () => {
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/clinicaldata" element={<Clinical />} />
+				<Route path="/iit" element={<Investigator />} />
+				<Route path="/medinfo" element={<MedInfo />} />
+				<Route path="/charitablegrant" element={<Char />} />
+			</Routes>
+		</BrowserRouter>
+	);
+};
+export default App;
